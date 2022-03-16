f62341e37d1870d681a4b7676692175892dda63d
piamayuga <44648337+jeboygwapo@users.noreply.github.com>
2022-03-16 17:53:40 +0800

    Login tite <3
    Login tite <3

--- Project/pcoo.system/model/DBQuery.php
+++ Project/pcoo.system/model/DBQuery.php
@@ -56,6 +56,25 @@
 
         }
         
+        public function index_login($username, $password) {
+            $sql = "SELECT id FROM employeeTbl WHERE username = '$username' and password = '$password'";
+            $result = mysqli_query($conn, $sql);
+            $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+            $active = $row['active'];
+
+            $count = mysqli_num_rows($result);
+            
+            // If result matched $username and $password, table row must be 1 row
+                
+            if($count == 1) {
+                session_register("username");
+                $_SESSION['login_user'] = $username;
+                
+                header("location: dashboard.html");
+            }else {
+                $error = "Your Login Name or Password is invalid";
+            }
+        }
             
         
     }
